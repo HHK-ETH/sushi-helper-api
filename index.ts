6d8de6fd3a0e88f5435174deb9b1d0b83225d5b1
HHK <hhk.contact@protonmail.com>
2022-12-05 15:59:44 -0600

    feat: add rate limiter of 10 request/2min

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import { formatUnits } from 'ethers/lib/utils';
 import express from 'express';
 import { getTotalSupply, StorageHelper } from './src';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const port = 3334;
+
+const limiter = rateLimit({
+  windowMs: 120_000, // 2 minutes
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 10 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 
 const storageHelper = StorageHelper.getInstance();
 
